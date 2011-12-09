f4c8617d6777390b31fa7ad68509c8818705f5e9
David Haslem <therabidbanana@gmail.com>
2011-12-09 01:18:58 -0500

    Remove unnecessary helpers

--- lib/html5/slim_rails/boilerplate_helper.rb
+++ lib/html5/slim_rails/boilerplate_helper.rb
@@ -2,27 +2,6 @@ module Html5
   module SlimRails
     module BoilerplateHelper
 
-      # Create a named haml tag to wrap IE conditional around a block
-      # http://paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither
-      def ie_tag(name=:body, attrs={}, &block)
-        attrs.symbolize_keys!
-        haml_concat("<!--[if lt IE 7]> #{ tag(name, add_class('ie6', attrs), true) } <![endif]-->".html_safe)
-        haml_concat("<!--[if IE 7]>    #{ tag(name, add_class('ie7', attrs), true) } <![endif]-->".html_safe)
-        haml_concat("<!--[if IE 8]>    #{ tag(name, add_class('ie8', attrs), true) } <![endif]-->".html_safe)
-        haml_concat("<!--[if gt IE 8]><!-->".html_safe)
-        haml_tag name, attrs do
-          haml_concat("<!--<![endif]-->".html_safe)
-          block.call
-        end
-      end
-
-      def ie_html(attrs={}, &block)
-        ie_tag(:html, attrs, &block)
-      end
-
-      def ie_body(attrs={}, &block)
-        ie_tag(:body, attrs, &block)
-      end
 
       def google_account_id
         ENV['GOOGLE_ACCOUNT_ID'] || google_config(:google_account_id)
